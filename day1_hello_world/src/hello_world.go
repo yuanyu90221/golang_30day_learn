78d0ba15ebd4a022e9005808f620ca79a1b97870
JsonLiang <json@cbx.io>
2019-09-15 19:49:43 +0800

    add detail

--- day1_hello_world/src/hello_world.go
+++ day1_hello_world/src/hello_world.go
@@ -5,5 +5,48 @@ import (
 )
 
 func main() {
+	// println
 	fmt.Println("hello 雷大")
+	// array
+	names := []string{"it", "home"}
+	fmt.Println(names)
+	// slice
+	game := "it home iron man"
+	fmt.Println(game[8:])
+	// switch case
+	gender := "female"
+	switch gender {
+	case "female":
+		fmt.Println("you are a girl")
+	case "male":
+		fmt.Println("you are a boy")
+	default:
+		fmt.Println("you are intersex")
+	}
+
+	// key value pairs
+	kvs := map[string]string{
+		"name":    "json",
+		"website": "https://yuanyu90221.github.io",
+	}
+
+	for key, value := range kvs {
+		fmt.Println(key, value)
+	}
+
+	type Post struct {
+		ID         int
+		Title      string
+		Author     string
+		Difficulty string
+	}
+
+	p := Post{
+		ID:         1011111,
+		Title:      "下班不講幹話多學點go",
+		Author:     "json",
+		Difficulty: "Beginner",
+	}
+
+	fmt.Println(p)
 }
