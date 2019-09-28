5b90555aab8da2d3f11b4731b71eb54aa928ee26
JsonLiang <json@cbx.io>
2019-09-29 01:38:33 +0800

    update

--- day4_container/src/goMain.go
+++ day4_container/src/goMain.go
@@ -95,4 +95,34 @@ func timeTest() {
 
 	// ---------Slice end, start list
 	fmt.Println("------------------------------")
+	t = time.Now()
+	l := list.New()
+	for i := 0; i < 1*100000*1000; i++ {
+		l.PushBack(1)
+	}
+	fmt.Println("List 新增元素耗費: " + time.Now().Sub(t).String())
+
+	t = time.Now()
+	for e := l.Front(); e != nil; e = e.Next() {
+	}
+	fmt.Println("走訪List耗費:" + time.Now().Sub(t).String())
+
+	var em *list.Element
+	i := 0
+	// 找到1/3處的元素
+	for e := l.Front(); e != nil; e = e.Next() {
+		i++
+		if i == l.Len()/3 {
+			em = e
+			break
+		}
+	}
+	// 因為是記算插入元素的速度, 所以忽略查找的時間
+	t = time.Now()
+	l.InsertAfter(2, em)
+	fmt.Println("List 的插入元素耗費 : " + time.Now().Sub(t).String())
+	// 比較刪除元素
+	t = time.Now()
+	l.Remove(em)
+	fmt.Println("List 的刪除元素耗費:" + time.Now().Sub(t).String())
 }
